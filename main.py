cd6a55db446049584fc2c39ebe7f798d1abd1977
realltd16 <79334394+realltd@users.noreply.github.com>
2021-09-30 02:39:49 +0900

    Update main.py
    update on github

--- main.py
+++ main.py
@@ -16,3 +16,6 @@ print("hello world 6")
 
 #add new in master branch
 print("hello world update on local master branch")
+
+#add new in master branch on git
+print("hello world2 update on remote master branch")
